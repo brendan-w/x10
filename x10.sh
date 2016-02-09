3f465c8b41d4d758072a00e9276a8dca9e6fc9f8
Brendan Whitfield <brendanw@windworksdesign.com>
2016-02-09 14:56:23 -0500

    emit logs and errors on stdout and stderr

--- x10.sh
+++ x10.sh
@@ -17,12 +17,12 @@ fi
 # try to acquire the lock X10 lock
 lock ~/locks/x10
 
-echo $(date) "-- X10 $@" >> ~/x10.log
+echo $(date) "-- X10 $@"
 
 # run each command 
 for i in seq 3
 do
-    br $@ --port=/dev/ttyUSB0 >> x10.log 2>&1
+    br $@ --port=/dev/ttyUSB0
     sleep 5
 done
 
