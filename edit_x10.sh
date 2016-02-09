5f780618f701bc3719e84245f90c4c59f487347e
Brendan Whitfield <brendanw@windworksdesign.com>
2016-02-09 15:19:01 -0500

    rewrote edit_x10

--- edit_x10.sh
+++ edit_x10.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-SCHED=./sched.x10
+# the default schedule file
+SCHEDULE=~/schedule.x10
+TMP_CRONTAB=/tmp/x10.crontab
 
-nano -Y sh $SCHED && ./compile_crontab.py $SCHED > sched.crontab && crontab sched.crontab
+# or, if the user specified a schedule file
+if [ "$#" -e 1 ]; then
+    SCHEDULE="$1"
+fi
+
+
+# let the user edit the schedule
+nano -Y sh $SCHEDULE
+
+# compile and write the X10 crontab
+./compile_crontab.py $SCHEDULE | cat ./header.crontab - > $TMP_CRONTAB
+
+# load our crontab, and delete the temporary file
+crontab $TMP_CRONTAB
+rm $TMP_CRONTAB
