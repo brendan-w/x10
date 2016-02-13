757c8528ecdf92e3ccff33245e0f00d83e654e4d
Brendan Whitfield <brendanw@windworksdesign.com>
2016-02-12 19:23:33 -0500

    kill off any lingering sunwaits when updating the crontab

--- edit_x10.sh
+++ edit_x10.sh
@@ -13,6 +13,14 @@ fi
 # let the user edit the schedule
 nano -Y sh $SCHEDULE
 
+# kill any lingering sunwaits, since they have probably been altered
+
+# kill the cron line first
+# if you only kill the sunwaits themselves, bash will
+# move on and execute the X10 commands
+pkill -f " -c sunwait" # the bash that cron spawned
+pkill -f "sunwait" # the actual sunwaits
+
 # compile and write the X10 crontab
 ./compile_crontab.py $SCHEDULE | cat ~/base.crontab ~/location.sh - > $TMP_CRONTAB
 
