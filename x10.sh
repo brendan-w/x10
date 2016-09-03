6c9cfe8feb0b129bd742c8e8c2f1e7014e491d80
Brendan Whitfield <brendanw@windworksdesign.com>
2016-09-03 19:32:31 -0400

    added basic argument checking

--- x10.sh
+++ x10.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-# if there are no arguments, show help
-if test $# -eq 0
-then
+help() {
     # print the intro text with command samples
     cat /etc/motd
     echo "x10 command usage:"
     echo "    x10 MODULE COMMAND [DIMLEVEL]"
     echo ""
-    echo "    MODULE: C5"
-    echo "    COMMAND: ON, OFF, DIM"
+    echo "    MODULE:   C5"
+    echo "    COMMAND:  ON, OFF, DIM"
     echo "    DIMLEVEL: a number between -12 and 12"
     echo ""
+}
+
+# if there are no arguments, show help
+if [ $# -lt 2 ] ; then
+    help
     exit 0
 fi
 
+module=$1
+command=$2
+
+# error check the input
+if [[ ! "ON|OFF|DIM" =~ ${command^^} ]] ; then
+    echo "Invalid command: $2" 1>&2
+    help
+    exit 0
+fi
+
+if [ ${#module} -lt 2 ] ; then
+    echo "Invalid module code: $1" 1>&2
+    help
+    exit 0
+fi
+
+house=${module:0:1}
+code=${module:1}
 
 # include the bash mutex
 . ./bashlock.sh
@@ -25,7 +46,10 @@ if ! lock ~/locks/x10 ; then
 	exit 1
 fi
 
+# for logging
 echo $(date) "-- X10 $@"
+
+# ---------- run command ----------
 
 # run each command multiple times to ensure successful transmission
 # ugly, but helps...
